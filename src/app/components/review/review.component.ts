0d8c3e0ccbea30fafdd61ca8177ed79570a5ce81
GDMaster4 <dalmasogianluca@gmail.com>
2025-02-16 14:29:10 +0100

    tolto currentUser

--- src/app/components/review/review.component.ts
+++ src/app/components/review/review.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Review } from '../../entities/review.entity';
 import { Router } from '@angular/router';
-import { User } from '../../entities/user.entity';
 
 @Component({
   selector: 'app-review',
@@ -13,9 +12,6 @@ export class ReviewComponent
   @Input()
   review:Review | null=null;
 
-  @Input()
-  currentUser:User | null = null;
-
   constructor(protected router:Router){}
 
   getStarsArray(rating: number): number[] {
@@ -24,9 +20,5 @@ export class ReviewComponent
 
   isReviewFilm() {
     return this.router.url.includes("films") ? true : false;
-  }
-
-  isCurrentUserAutor() {
-    return this.currentUser!.id! == this.review!.Autore.id! ? true : false;
   }
 }
