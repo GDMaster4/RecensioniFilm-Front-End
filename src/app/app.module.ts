bd70a88139c42e9a7a876bac27f10c55895bd137
GDMaster4 <dalmasogianluca@gmail.com>
2025-02-22 11:07:03 +0100

    BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
@@ -43,6 +44,7 @@ import { ProfiloComponent } from './pages/profilo/profilo.component';
     ProfiloComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
